4a90262d89c453b2a0a8f67a41f1128cac57c5ee
ArnaldoSoares <arnaldo.sosilva@gmail.com>
2022-06-30 22:33:59 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const card = document.querySelectorAll('.card');
+let hasFlippedcard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedcard){
+        hasFlippedcard = true;
+        firstCard = this;
+        return;
+    }
+    
+    secondCard = this;
+    hasFlippedcard = false;
+    checkForMatch();
+
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedcard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    card.forEach((card) =>{
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+card.forEach((card) => {
+    card.addEventListener('click', flipCard)    
+});
+
+
